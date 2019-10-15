06c3a0c06b1bf39a87405b5cddda9d951ba0cf71
Ad Schellevis <ad@opnsense.org>
2019-10-15 20:56:51 +0200

    pfSenseImport: complete alias import and validation

--- devel/pfSenseImport/OPNsense/pfSenseImport/Alias.php
+++ devel/pfSenseImport/OPNsense/pfSenseImport/Alias.php
@@ -28,18 +28,106 @@
 */
 namespace OPNsense\pfSenseImport;
 
+use \OPNsense\Core\Config;
 
 class Alias
 {
     private $sourceXml = null;
+    private $importErrors = array();
+    private $insertCount = 0;
+    private $updateCount = 0;
     public function __construct($source)
     {
         if (file_exists($source)) {
             $this->sourceXml = simplexml_load_file($source);
         }
     }
-    public function load()
+    public function import()
     {
-        $alias = new \OPNsense\Firewall\Alias();
+        $this->insertCount = 0;
+        $this->updateCount = 0;
+        $this->importErrors = array();
+        $aliasImportMap = array();
+        $aliasUuidMap = array();
+        $aliasMdl = new \OPNsense\Firewall\Alias();
+        if (!empty($this->sourceXml->aliases) && !empty($this->sourceXml->aliases->alias)) {
+            Config::getInstance()->lock();
+            foreach ($this->sourceXml->aliases->alias as $srcAlias) {
+                if (empty($srcAlias->name)) {
+                    $this->importErrors[] = array(
+                        "name" => null,
+                        "details" => json_encode($srcAlias),
+                        "message" => "missing alias name"
+                      );
+                    continue;
+                } elseif (!empty($aliasImportMap[(string)$srcAlias->name])) {
+                    $this->importErrors[] = array(
+                        "name" => (string)$srcAlias->name,
+                        "details" => json_encode($srcAlias),
+                        "message" => "duplicate alias name"
+                    );
+                    continue;
+                } elseif ($alias=$aliasMdl->getByName((string)$srcAlias->name)) {
+                    $aliasImportMap[(string)$srcAlias->name] = $alias;
+                    $this->updateCount++;
+                } else {
+                    $alias = $aliasMdl->aliases->alias->Add();
+                    $aliasImportMap[(string)$srcAlias->name] = $alias;
+                    $this->insertCount++;
+                }
+                // update target object
+                $alias->name = (string)$srcAlias->name;
+                $alias->type = (string)$srcAlias->type;
+                if ($srcAlias->url) {
+                    // url content only contains a single item
+                    $alias->content = (string)$srcAlias->url;
+                } elseif ($srcAlias->aliasurl) {
+                    // aliasurl in legacy config could consist of multiple <aliasurl> entries
+                    $content = array();
+                    foreach ($srcAlias->aliasurl as $url) {
+                        $content[] = (string)$url;
+                    }
+                    $alias->content = implode("\n", $content);
+                } elseif ($srcAlias->address) {
+                    // address entries
+                    $alias->content = str_replace(" ", "\n", trim((string)$srcAlias->address));
+                }
+                if ($srcAlias->proto) {
+                    $alias->proto = (string)$srcAlias->proto;
+                }
+                if ($srcAlias->updatefreq) {
+                    $alias->updatefreq = (string)$srcAlias->updatefreq;
+                }
+                $aliasUuidMap[$alias->getAttribute('uuid')] = (string)$srcAlias->name;
+            }
+            \OPNsense\Firewall\Util::attachAliasObject($aliasMdl);
+            foreach ($aliasMdl->performValidation() as $msg) {
+                $parts = explode('.', $msg->getField());
+                $uuid = $parts[count($parts)-2];
+                $this->importErrors[] = array(
+                    "uuid" => $uuid,
+                    "name" => $aliasUuidMap[$uuid],
+                    "details" => null,
+                    "message" => $msg->getMessage()
+                );
+            }
+            // remove invalid entries from set
+            foreach ($this->importErrors as $error) {
+                if (!empty($error['uuid'])) {
+                    $aliasMdl->aliases->alias->del($error['uuid']);
+                }
+            }
+            $aliasMdl->serializeToConfig();
+            Config::getInstance()->save();
+        }
+    }
+
+    public function getReport()
+    {
+        return array(
+            "inserted" => $this->insertCount,
+            "updated" => $this->updateCount,
+            "errors" => $this->importErrors,
+        );
     }
 }

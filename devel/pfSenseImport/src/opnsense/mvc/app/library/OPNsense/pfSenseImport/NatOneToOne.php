1af1838e18103c77c269b894465a6950c5adbfa9
Ad Schellevis <ad@opnsense.org>
2019-11-07 18:01:05 +0100

    pfSenseImport add one to one nat and filter rules

---
+++ devel/pfSenseImport/src/opnsense/mvc/app/library/OPNsense/pfSenseImport/NatOneToOne.php
@@ -0,0 +1,81 @@
+<?php
+/**
+*    Copyright (C) 2019 Deciso B.V.
+*
+*    All rights reserved.
+*
+*    Redistribution and use in source and binary forms, with or without
+*    modification, are permitted provided that the following conditions are met:
+*
+*    1. Redistributions of source code must retain the above copyright notice,
+*       this list of conditions and the following disclaimer.
+*
+*    2. Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*
+*    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+*    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+*    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+*    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+*    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+*    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+*    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+*    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*    POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+namespace OPNsense\pfSenseImport;
+
+use \OPNsense\Core\Config;
+use \OPNsense\Firewall\Util;
+
+class NatOneToOne extends ImportType
+{
+
+    private function genRuleId($rule)
+    {
+        $result = (!empty($rule->interface) ? $rule->interface : "?") . "|";
+        $result .= !empty($rule->source) ? $this->pconfigToString($rule->source) : "?";
+        $result .= "->";
+        $result .= !empty($rule->destination) ? $this->pconfigToString($rule->destination) : "?";
+        return $result;
+    }
+
+    public function import()
+    {
+        if (!empty($this->sourceXml->nat) && !empty($this->sourceXml->nat->onetoone)) {
+            Config::getInstance()->lock();
+            foreach ($this->sourceXml->nat->onetoone as $srcRule) {
+                $this_id = $this->genRuleId($srcRule);
+                if (!$this->hasInterface($srcRule->interface) && !$this->hasInterfaceGroup($srcRule->interface)) {
+                    $this->importErrors[] = array(
+                        "name" => $this_id,
+                        "details" => json_encode($srcRule),
+                        "message" => "Interface not configured"
+                      );
+                    continue;
+                }
+                // add / update rule
+                $target = Config::getInstance()->object();
+                $nat = isset($target->nat) ? $target->nat : $target->addChild("nat");
+                $natEntry = null;
+                foreach ($nat->children() as $rule) {
+                    if ($this->genRuleId($rule) == $this_id) {
+                        $natEntry = $rule;
+                        break;
+                    }
+                }
+                if ($natEntry == null) {
+                    $natEntry = $nat->addChild("onetoone");
+                    $this->insertCount++;
+                } else {
+                    $this->updateCount++;
+                }
+                $this->replaceXmlNode($srcRule, $natEntry);
+            }
+            Config::getInstance()->save();
+        }
+    }
+}
